5364f7b21f03b2327c87842c0767faf0ef3fbccb
elf <elf@mail.com>
2025-03-01 22:25:25 +0000

    add-discovery-service

--- gateway-service/src/main/java/org/example/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/example/GatewayServiceApplication.java
@@ -3,10 +3,25 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class,args);
+    }
+
+
+    //to use dynamic routing, we must first implement discovery because rdc is
+    //taking the URL request ex http://8888/SERVICE-NAME/api/some-work then he know that
+    //he must search in discovery a service called SERVICE-NAME to have the full URL
+    //of that service called.
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc,
+                                                  DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 }
