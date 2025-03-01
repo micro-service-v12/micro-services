625333973f2ff9419db285a32614446e4f883353
elf <elf@mail.com>
2025-03-01 15:33:35 +0000

    sute

---
+++ customer-service/src/main/java/org/example/CustomerServiceApplication.java
@@ -0,0 +1,30 @@
+package org.example;
+
+
+import org.example.entities.Customer;
+import org.example.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class,args);
+    }
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder()
+                    .name("Taoufik").email("tao@gmail.com")
+                    .build());
+
+            customerRepository.save(Customer.builder()
+                    .name("Said").email("sai@gmail.com")
+                    .build());
+        };
+    }
+}
